c5ca13daaa0fc58f2fb31e1706df53e3fe805882
Yoonoh <xkakak142@naver.com>
2021-03-20 16:55:04 +0900

    add SignUp Function

--- servers/routes/toys.js
+++ servers/routes/toys.js
@@ -4,12 +4,12 @@ const router = express.Router();
 var path = require("path");
 var java = require("java");
 
-java.classpath.push(path.resolve('./lib/ver0.85.jar'));
-var DBClass = java.import('manager.GameManager');
-var gm = new DBClass();
+// java.classpath.push(path.resolve('./lib/ver0.85.jar'));
+// var DBClass = java.import('manager.GameManager');
+// var gm = new DBClass();
 
-router.get('/', (req, res) => {
-    res.send(gm.getCharacterSync("00000cf0b1e487ad9b6e8386fd1d34f3f0b6e018dd7c0835c6e9e2d404269510"));
-});
+// router.get('/', (req, res) => {
+//     res.send(gm.getCharacterSync("00000cf0b1e487ad9b6e8386fd1d34f3f0b6e018dd7c0835c6e9e2d404269510"));
+// });
 
 module.exports = router;
